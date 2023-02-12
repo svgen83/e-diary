efbe996edece6513e1733b9cbeea0d38b2a0d483
svgen83 <61458549+svgen83@users.noreply.github.com>
2023-02-12 19:20:22 +0400

    несколько изменений
    добавлена константа,
    добавлены функции получения учеников и уроков
    исправлены опечатки

--- scripts.py
+++ scripts.py
@@ -1,52 +1,55 @@
 import random
 from datacenter.models import Schoolkid, Mark, Lesson
 from datacenter.models import Chastisement, Commendation
-from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
+
+PRAISES = ['Молодец!', 'Хвалю!',
+           'Ты меня приятно удивил!',
+           'Ты растешь над собой!', 'Так держать!']
 
 
-def fix_marks(pupil_name, grade):
+def get_pupil(pupil_name):
     try:
-        pupil = Schoolkid.objects.get(full_name__contains=pupil_name)
-        pupil_marks = Mark.objects.filter(schoolkid=pupil,
-                                          points__lte=3)
-        pupil_marks.update(points=grade)
-        print('Оценки исправлены')
+        return Schoolkid.objects.get(full_name__contains=pupil_name)
     except MultipleObjectsReturned:
-        print('Найдено несколько учеников с таким именем. Уточните запрос')
+        print ('Найдено несколько учеников с таким именем. Уточните запрос')
     except ObjectDoesNotExist:
-        print('Ученик не найден. Уточните запрос')
+        print ('Ученик не найден. Уточните запрос')
 
 
-def remove_chastisements(pupil_name):
-    try:
-        pupil = Schoolkid.objects.get(full_name__contains=pupil_name)
-        chastisement = Chastisement.objects.filter(schoolkid=pupil)
-        chastisement.delete()
-        print('Замечания удалены')
-    except MultipleObjectsReturned:
-        print('Найдено несколько учеников с таким именем. Уточните запрос')
-    except ObjectDoesNotExist:
-        print('Ученик не найден. Уточните запрос')
-
-
-def create_commendation(pupil_name, lesson_subject):
-    try:
-        pupil = Schoolkid.objects.get(full_name__contains=pupil_name)
-        lesson = Lesson.objects.filter(
+def get_lesson(lesson_subject, pupil):
+    lesson = Lesson.objects.filter(
             year_of_study=pupil.year_of_study,
             group_letter=pupil.group_letter,
             subject__title__contains=lesson_subject
-            ).order_by('-date').last()
-        prases = ['Молодец!', 'Хвалю!',
-                  'Ты меня приятно удивил!',
-                  'Ты растешь над собой!', 'Так держать!']
-        Commendation.objects.create(teacher=lesson.teacher,
+            ).order_by('-date').first()
+    if lesson:
+        return lesson
+    else: raise ObjectDoesNotExist ('Предмет не найден')
+
+
+def fix_marks(pupil_name, grade):
+    pupil = get_pupil(pupil_name)
+    pupil_marks = Mark.objects.filter(schoolkid=pupil,
+                                      points__lte=3)
+    pupil_marks.update(points=grade)
+    print('Оценки исправлены')
+    
+
+def remove_chastisements(pupil_name):
+    pupil = get_pupil(pupil_name)
+    chastisement = Chastisement.objects.filter(schoolkid=pupil)
+    chastisement.delete()
+    print('Замечания удалены')
+
+
+def create_commendation(pupil_name, lesson_subject):
+    pupil = get_pupil(pupil_name)
+    lesson = get_lesson(lesson_subject, get_pupil(pupil_name))       
+    Commendation.objects.create(teacher=lesson.teacher,
                                     created=lesson.date,
                                     subject=lesson.subject,
-                                    text=random.choice(prases),
+                                    text=random.choice(PRAISES),
                                     schoolkid=pupil)
-        print('Создан положительный отзыв учителя')
-    except MultipleObjectsReturned:
-        print('Найдено несколько учеников с таким именем. Уточните запрос')
-    except ObjectDoesNotExist:
-        print('Ученик не найден. Уточните запрос')
+    print('Создан положительный отзыв учителя')
+
